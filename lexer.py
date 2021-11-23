cf11305c53f04052f01d2c7ee6c0612501c22651
NayotamaPradipta <n4y0t4m4@gmail.com>
2021-11-23 22:15:45 +0700

    Add lexer and rules

--- lexer.py
+++ lexer.py
@@ -1,1 +1,67 @@
-# File untuk lexer 
+import re
+import os
+import lexerRules
+import CYKparser as parser
+
+
+# menerima error lexer digunakan pada kelas Changing fungsi token dnegna mengoper-oper objeknya
+# Jika tidak ada pasangan dalam matching sehingga mengembalikan string error position dan berhenti LEXERERROR, tapi ngga wajib, buat meriksanya nanti
+
+class Changing(object):
+    def __init__(self, rules, skip_whitespace):
+        idx = 1
+        regex_parts = []
+        self.tipekelompok = {}
+
+        for regex, type in rules:
+            
+            #merupakan nama state dari finite automata.
+            kelompok = 'Kelompok%s' % idx
+            #membuat list-list dari states yang ada
+            regex_parts.append('(?P<%s>%s)' % (kelompok, regex))
+
+            self.tipekelompok[kelompok] = type
+            # membaca lexer rules perbaris nambah 1, jika valid.
+            idx += 1
+
+        self.regex = re.compile('|'.join(regex_parts))
+        self.skip_whitespace = skip_whitespace
+        self.re_ws_skip = re.compile('\S')
+
+
+    def token(self):
+
+        
+        #mengembalikan None apabila di buffer tidak ada apa-apa sehingga panjang buffer 0 atau kurang dari 0.
+        if self.pos >= len(self.buf):
+            return None
+        #jika buffer lebih besar dari posnya.
+        else:
+            # mencari pasangannya atau group kelompoknya yang benar seperti ";" terminalnya adalah SEMICOLON
+            n = self.regex.match(self.buf, self.pos)
+
+            if n:
+                kelompok = n.lastgroup
+                tipe = self.tipekelompok[kelompok]
+                self.pos = n.end()
+                if (tipe == 'WHITESPACE') :
+                    return ''
+                # mengembalikan jenis dari input 
+                return tipe
+
+    def tokens(self,buf):
+        #menerima input
+        self.buf = buf
+        self.pos = 0
+        # eksekusi dimulai dari dimana berhenti, jadi tidak menghapus fungsi local variabelnya, tetapi menyimpannya.
+        # seperti word machine. Tapi menyimpan panjangnya, tidak mengulang dari nol sampai ketemu none dari token
+        while True:
+            hasiltoken = self.token()
+            if hasiltoken is None: 
+                break
+            yield hasiltoken
+
+CYK = parser.Parser('grammar.txt', " COMMENT ")
+
+
+
---
+++ lexerRules.py
@@ -0,0 +1,70 @@
+lexerRules = [
+    # Number and float
+    (r'\d+',             'NUMBER'),
+    (r'\d+.+\d',         'FLOAT'),
+    # Identifier (Variable, Function, class, object, module, dll)
+    (r'[a-zA-Z_]\w+',    'IDENTIFIER'),
+    # Mathematical Operators
+    (r'\+',              'PLUS'),
+    (r'\-',              'MINUS'),
+    (r'\*',              'MULTIPLY'),
+    (r'\/',              'DIVIDE'),
+    (r'\*\*',            'POWER'),
+    (r'\%',              'MOD'),
+    # Punctuation
+    (r'\(',              'LP'),
+    (r'\)',              'RP'),
+    (r'\[',              'LB'),
+    (r'\]',              'RB'),
+    (r',',               'COMA'),
+    (r':',               'COLON'),
+    (r';',               'SEMICOLON'),
+    (r'\#.*',            'COMMENT'),
+
+    # Assign value
+    (r'=',               'EQUALS'),
+    # Comparison operators
+    (r'==',              'DOUBLEEQUAL'),
+    (r'!=',              'NOT_EQUAL'),
+    (r'>',               'GREATER_THAN'),
+    (r'<',               'LESS_THAN'),
+    (r'>=',              'GREATER_OR_EQUAL_THAN'),
+    (r'<=',              'LESS_OR_EQUAL_THAN'),
+    # Whitespace and newline
+    (r'\s',              'WHITESPACE'),
+    (r'\n',              'NEWLINE'),
+    # String
+    ('\".*\"',           'STRING'),
+    ('\'.*\'',           'STRING'),
+
+    # Python keywords yang harus terdaftar
+    (r'False',           'FALSE'),
+    (r'class\s',         'CLASS'),
+    (r'is\s',            'IS'),
+    (r'return\s',        'RETURN'),
+    (r'None',            'NONE'),
+    (r'continue\n',      'CONTINUE'),
+    (r'for\s',           'FOR'),
+    (r'True',            'TRUE'),
+    (r'def\s',           'DEF'),
+    (r'from\s',          'FROM'),
+    (r'while',           'WHILE'),
+    (r'and',             'AND'),
+    (r'not',             'NOT'),
+    (r'with\s',          'WITH'),
+    (r'as\s',            'AS'),
+    (r'elif',            'ELIF'),
+    (r'elif\(',          'ELIF LP'),
+    (r'if\s',            'IF'),
+    (r'if\(',            'IF LP'),
+    (r'\sor\s',          'OR'),
+    (r'else',            'ELSE'),
+    (r'import\s',        'IMPORT'),
+    (r'pass',            'PASS'),
+    (r'break\n',         'BREAK NEWLINE'),
+    (r'break\w',         'BREAK ERR'),
+    (r'in\s',            'IN'),
+    (r'raise\s',         'RAISE'),
+    # Tambahan
+    (r'print',           'PRINT'),
+]
